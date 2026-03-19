042eac59c01405b3a23217e9e14ee81d60796059
Dryelp <andryelp09@gmail.com>
2026-03-18 23:57:52 -0300

    Atualizar o sw.js

--- serivicopro/sw.js
+++ serivicopro/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'autonomopro-v6';
+const CACHE = 'autonomopro-v7';
 // Atualizar este número a cada novo deploy para forçar atualização nos dispositivos
 const FILES = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
